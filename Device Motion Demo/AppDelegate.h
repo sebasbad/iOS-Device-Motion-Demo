569cba8dca51e4e21ecb825a21c82aae45d20200
Sebastián Badea <sebastian.badea@infojobs.net>
2016-04-18 18:13:49 +0200

    Initial Commit

---
+++ Device Motion Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Device Motion Demo
+//
+//  Created by Sebastián Badea on 18/4/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Device Motion Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Device Motion Demo
+//
+//  Created by Sebastián Badea on 18/4/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
