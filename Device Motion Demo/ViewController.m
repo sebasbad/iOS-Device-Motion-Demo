fcdfed253a3235da44f11a8d1ab0c53f4abcf06a
Sebastián Badea <sebastian.badea@infojobs.net>
2016-04-19 20:29:51 +0200

    Updated main view controller to set image view image

--- Device Motion Demo/ViewController.m
+++ Device Motion Demo/ViewController.m
@@ -19,7 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.imageView.image = [UIImage imageNamed:@"dog.jpg"];
 }
 
 @end
