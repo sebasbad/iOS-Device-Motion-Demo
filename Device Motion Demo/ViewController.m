175c814b86d42ec329adcfc5d16262ddad58b716
Sebastián Badea <sebastian.badea@infojobs.net>
2016-04-19 20:30:53 +0200

    Updated main view controller to use core motion to rotate the image view using the device motion updates

--- Device Motion Demo/ViewController.m
+++ Device Motion Demo/ViewController.m
@@ -13,6 +13,8 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
+@property (strong, nonatomic) CMMotionManager *coreMotionManager;
+
 @end
 
 @implementation ViewController
@@ -21,6 +23,28 @@
     [super viewDidLoad];
 
     self.imageView.image = [UIImage imageNamed:@"dog.jpg"];
+    
+    self.coreMotionManager = [[CMMotionManager alloc] init];
+    [self.coreMotionManager startDeviceMotionUpdates];
+    
+    self.coreMotionManager.accelerometerUpdateInterval = 0.01; //seconds
+    
+    ViewController * __weak weakSelf = self;
+    
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    
+    [self.coreMotionManager startDeviceMotionUpdatesToQueue:queue withHandler:^(CMDeviceMotion *deviceMotion, NSError *error) {
+        /* do work here */
+        double x = deviceMotion.gravity.x;
+        double y = deviceMotion.gravity.y;
+        
+        double rotation = -atan2(x, y);
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            /* Update UI here */
+            weakSelf.imageView.transform = CGAffineTransformMakeRotation(rotation);
+        }];
+    }];
 }
 
 @end
